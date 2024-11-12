4199f79f1e941ec3febd5bdcfde6445ca4e9235e
gshjjp <1264916503@qq.com>
2024-11-12 17:45:43 +0800

    1

--- utils/read_data.py
+++ utils/read_data.py
@@ -8,7 +8,7 @@ import  yaml
 f = open("../config/data.yaml",encoding='utf-8')
 
 data = yaml.safe_load(f)
-print("---------------")
+print("------------------")
 print(data)
 print(data['hero'])
 print(data['heros_name'])
